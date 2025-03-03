7efcd48bef990bb2ec125443f8c78ed552dc8193
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2025-03-03 21:25:19 +0000

    played about with epochs - no real changes

--- oe2.py
+++ oe2.py
@@ -1,7 +1,12 @@
 class Perceptron:
     def __init__(self, num_inputs=1, learning_rate=0.1):
         self.num_inputs = num_inputs
-        self.weights = [0] * num_inputs
+        # self.weights = [0] * num_inputs original
+        # self.weights = [6.1999999999947235] 1st 
+        #self.weights = [3.9999999999893956] #2nd
+        # self.bias = -31.79999999999992 1st
+        #self.bias = -31.79999999999992 #
+        self.weights = [1]
         self.bias = 0
         self.learning_rate = learning_rate
         
@@ -10,26 +15,32 @@ class Perceptron:
         weighted_sum = self.bias
         for i in range(self.num_inputs):
             weighted_sum += self.weights[i] * inputs[i]
+            print(f'Input: {inputs[i]}, Weighted sum: {weighted_sum}, Self.Weights, {self.weights}, Bias: {self.bias}')
         return weighted_sum
     
     def activation(self, weighted_sum):
         # print(f'weighted sum: {weighted_sum}')
         return 1 if weighted_sum >= 0 else -1
     
-    def training(self, training_set, max_epochs=100000, learning_rate=0.001):
+    def training(self, training_set, max_epochs=100, learning_rate=0.1):
         for epoch in range(max_epochs):
             total_error = 0
 
             for inputs, actual in training_set:
-                prediction = self.activation(self.weighted_sum(inputs))
+                # print(f'input: {inputs}, actual: {actual}')
+                weighted_sum = self.weighted_sum(inputs)
+                prediction = self.activation(weighted_sum)
+                # prediction = self.activation(self.weighted_sum(inputs))
                 error = actual - prediction
                 total_error += abs(error)
 
+                # print(f'input: {inputs},  prediction: {prediction}, actual: {actual}, weighted_sum: {weighted_sum}, error: {error}, total_error: {total_error}, bias: {self.bias}')
+
                 for i in range(self.num_inputs):
-                    self.weights[i] += learning_rate * error * inputs[i]
+                    self.weights[i] += learning_rate * error * inputs[i] 
                 self.bias += learning_rate * error
             
-            # print(f'Epoch {epoch + 1}, Total error: {total_error}, Weights: {self.weights}, Bias: {self.bias}')
+            print(f'Epoch {epoch + 1}, Total error: {total_error}, Weights: {self.weights}, Bias: {self.bias}')
 
             if total_error == 0:
                 print(f'Converged after {epoch + 1} epocs')
@@ -37,16 +48,18 @@ class Perceptron:
 
         if epoch == max_epochs:
             print('Maximum epochs reached, the training did not converge')
-        
+        print(f'Return value of weights: {self.weights}')
         return self.weights
     
+# ([-5], -1),
+#     ([-4], 1),
+#     ([-3], -1),
+#     ([-2], 1),
+#     ([-1], -1),
+#     ([0], 1),
+    
 training_data_even_odd = [
-    ([-5], -1),
-    ([-4], 1),
-    ([-3], -1),
-    ([-2], 1),
-    ([-1], -1),
-    ([0], 1),
+    
     ([1], -1),
     ([2], 1),
     ([3], -1),
@@ -79,15 +92,40 @@ training_data_even_odd = [
     ([30], 1)
 ]
 
-perceptron_2 = Perceptron(num_inputs=1)
+# training_data_even_odd = [
+#     ([0], 1),
+#     ([2], 1), 
+#     ([4], 1),
+#     ([6], 1),
+#     ([8], 1),
+#     ([10], 1),
+#     ([12], 1),
+#     ([14], 1),
+#     ([16], 1),
+#     ([18], 1),
+#     ([20], 1),
+#     ([1], -1),
+#     ([3], -1),
+#     ([5], -1),
+#     ([7], -1),
+#     ([9], -1),
+#     ([11], -1),
+#     ([13], -1),
+#     ([15], -1),
+#     ([17], -1),
+#     ([19], -1),
+# ]
+
+perceptron_2 = Perceptron(num_inputs=1, learning_rate=0.1)
 perceptron_2.training(training_data_even_odd)
 
 result_2 = perceptron_2.activation(perceptron_2.weighted_sum([1]))
 result_3 = perceptron_2.activation(perceptron_2.weighted_sum([121]))
 
-# print(result_2, result_3)
+print('Results 2 and 3', result_2, result_3)
+# -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 
 
-test_nums = [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+test_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 for i in test_nums:
     result = perceptron_2.activation(perceptron_2.weighted_sum([i]))
